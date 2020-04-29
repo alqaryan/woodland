f4b0cfffa51ca8aa62a831a70a36e1f6f6329cdd
alqaryan <47994443+alqaryan@users.noreply.github.com>
2020-04-29 02:58:55 -0500

    Fb Login feature

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -1,89 +1,109 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Alert, StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { TouchableOpacity } from "react-native-gesture-handler";
 //import fire from "./config/fire.js";
 import * as firebase from "firebase";
+import Expo from "expo";
 
 const Stack = createStackNavigator();
 
 export default function SignInScreen({ navigation }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    function login(email, password){
-        firebase.auth().signInWithEmailAndPassword(email, password)
-        .catch(err => {
-            setErrorMessage(err.message);
-            console.log(err.message);
-            alert(err.message);
-        });
-    };
+  function login(email, password) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        setErrorMessage(err.message);
+        console.log(err.message);
+        alert(err.message);
+      });
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.greeting}>
-                {'Welcome to Woodland!\n Sign In to discover'}
-            </Text>
+  FBID = "222013642554489";
+  FBLogin = async () => {
+    const {
+      type,
+      token,
+    } = await Expo.Facebook.logInWithReadPermissionsAsync(FBID, {
+      permissions: ["public_profile", "email"],
+    });
 
-            <View style={styles.errorMessage}>
-                <Text></Text>
-            </View>
+    if (type === "success") {
+      try {
+        firebase.auth().signInWithCredential(token);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert(type);
+    }
+  };
 
-            <View style={styles.form}>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>
+        {"Welcome to Woodland!\n Sign In to discover"}
+      </Text>
 
-                <View>
-                    <Text style={styles.inputTitle}>
-                        Email Address
-                    </Text>
-                    <TextInput
-                        autoCapitalize="none"
-                        style={styles.input}
-                        value={username}
-                        onChangeText={setUsername}
-                    />
-                </View>
+      <View style={styles.errorMessage}>
+        <Text></Text>
+      </View>
 
-                <View style={{ marginTop: 32 }}>
-                    <Text style={styles.inputTitle}>
-                        Password
-                    </Text>
-                    <TextInput
-                        autoCapitalize="none"
-                        style={styles.input}
-                        value={password}
-                        secureTextEntry
-                        onChangeText={setPassword}
-                    />
-                </View>
-            </View>
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.inputTitle}>Email Address</Text>
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            value={username}
+            onChangeText={setUsername}
+          />
+        </View>
 
-            <TouchableOpacity
-                onPress={() => login(username, password)}
-                style={styles.button}
-            >
-                <Text style={{color: "#FFF", fontWeight:"500"}}>
-                    Sign in
-                </Text>
-            </TouchableOpacity>
+        <View style={{ marginTop: 32 }}>
+          <Text style={styles.inputTitle}>Password</Text>
+          <TextInput
+            autoCapitalize="none"
+            style={styles.input}
+            value={password}
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+        </View>
+      </View>
 
-            <TouchableOpacity
-                onPress={ () => navigation.navigate("Register")}
-                style={{alignSelf: "center", marginTop:15}}
-            >
-                <Text style={{color : "#414959" , fontSize: 15}}>
-                    New Explorer? <Text style={{color: "orange", fontWeight: "500"}}>Sign up</Text>
-                </Text>
-            </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => login(username, password)}
+        style={styles.button}
+      >
+        <Text style={{ color: "#FFF", fontWeight: "500" }}>Sign in</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => FBLogin()} style={styles.FBbutton}>
+        <Text style={{ color: "#FFF", fontWeight: "500" }}>
+          Facebook Sign in
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Register")}
+        style={{ alignSelf: "center", marginTop: 15 }}
+      >
+        <Text style={{ color: "#414959", fontSize: 15 }}>
+          New Explorer?{" "}
+          <Text style={{ color: "orange", fontWeight: "500" }}>Sign up</Text>
+        </Text>
+      </TouchableOpacity>
     </View>
-      );
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -91,28 +111,28 @@ const styles = StyleSheet.create({
   },
 
   greeting: {
-      marginTop:32,
-      fontSize: 18,
-      fontWeight: "400",
-      textAlign: "center"
+    marginTop: 32,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "center",
   },
 
   errorMessage: {
-      height: 72,
-      alignItems: 'center',
-      justifyContent: "center",
-      marginHorizontal: 30
+    height: 72,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 30,
   },
 
   form: {
     marginBottom: 48,
-    marginHorizontal: 30
+    marginHorizontal: 30,
   },
 
   inputTitle: {
     color: "#8A8F9E",
     fontSize: 10,
-    textTransform: "uppercase"
+    textTransform: "uppercase",
   },
 
   input: {
@@ -129,14 +149,22 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     height: 52,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+  },
+
+  FBbutton: {
+    marginHorizontal: 30,
+    backgroundColor: "#3b5998",
+    borderRadius: 4,
+    height: 52,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
-
 // added below for Jest testing
 try {
-    module.exports = SignInScreen;
-  } catch (error) {
-    console.log('we caught an error');
-  }
+  module.exports = SignInScreen;
+} catch (error) {
+  console.log("we caught an error");
+}
